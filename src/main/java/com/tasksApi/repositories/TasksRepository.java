4811f10ca5f9c2c7d3ea92d74a10b5266b7e37f9
Paul Richard <paulrpm@gmail.com>
2024-09-10 23:18:55 -0300

    feat: created methods to handle assigned and unassigned tasks

--- src/main/java/com/tasksApi/repositories/TasksRepository.java
+++ src/main/java/com/tasksApi/repositories/TasksRepository.java
@@ -1,5 +1,6 @@
 package com.tasksApi.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
 import org.springframework.stereotype.Repository;
@@ -9,4 +10,10 @@ import com.tasksApi.model.Tasks;
 @SuppressWarnings("rawtypes")
 @Repository
 public interface TasksRepository extends CrudRepository<Tasks, Integer>, QueryByExampleExecutor {
+
+    @Query("FROM task t LEFT JOIN task_assignees ta ON ta.task = t.id WHERE ta.id IS NULL")
+    public Iterable<Tasks> hasNoAssignees();
+
+    @Query("FROM task t LEFT JOIN task_assignees ta ON ta.task = t.id WHERE ta.id IS NOT NULL")
+    public Iterable<Tasks> hasAssignees();
 }
